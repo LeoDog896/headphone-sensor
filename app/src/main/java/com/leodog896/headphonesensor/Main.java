5a25e7f3628423a75f514aa2168d4cefdce05643
Tristan F <LeoDog896@hotmail.com>
2023-03-25 21:31:00 -0400

    refactor: better code cleanup

--- app/src/main/java/com/leodog896/headphonesensor/Main.java
+++ app/src/main/java/com/leodog896/headphonesensor/Main.java
@@ -22,6 +22,7 @@ public class Main extends AppCompatActivity {
             "Search up fixes on your search engine.",
             "Use Google Support."
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,30 +31,43 @@ public class Main extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         textview = (TextView) findViewById(R.id.textView1);
-        trouble =  (TextView) findViewById(R.id.textViewTrouble) ;
-        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        if(audioManager.isWiredHeadsetOn()) textview.setText("Headset is plugged.");
-        else if (!audioManager.isWiredHeadsetOn()) textview.setText("Headset is unplugged.");
-        else textview.setText("Headset has an unknown state, update it.");
+        trouble = (TextView) findViewById(R.id.textViewTrouble);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        if (audioManager.isWiredHeadsetOn())
+            textview.setText("Headset is plugged.");
+        else if (!audioManager.isWiredHeadsetOn())
+            textview.setText("Headset is unplugged.");
+        else
+            textview.setText("Headset has an unknown state, update it.");
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(
                 v -> {
                     if (audioManager.isWiredHeadsetOn()) {
-                        textview.setText("Headset is plugged."); again = 0; trouble.setText("");
+                        textview.setText("Headset is plugged.");
+                        again = 0;
+                        trouble.setText("");
                     } else if (!audioManager.isWiredHeadsetOn()) {
                         trouble.setText(getRandom(troubleshoots));
-                        if (again >= 100) textview.setText("Try physically repairing your phone.");
-                        else if (again >= 10) textview.setText("Headset is unplugged, try troubleshooting more.");
-                        else if (again >= 1) textview.setText("Headset is unplugged, try troubleshooting again.");
-                        else textview.setText("Headset is unplugged, try troubleshooting.");
+
+                        if (again >= 100)
+                            textview.setText("Try physically repairing your phone.");
+                        else if (again >= 10)
+                            textview.setText("Headset is unplugged, try troubleshooting more.");
+                        else if (again >= 1)
+                            textview.setText("Headset is unplugged, try troubleshooting again.");
+                        else
+                            textview.setText("Headset is unplugged, try troubleshooting.");
+                        
                         again++;
                     } else {
                         textview.setText("Headset has an unknown state, update it.");
                         trouble.setText("");
                     }
-                }
-        );
+                });
     }
+
     public static String getRandom(String[] array) {
         int rnd = new Random().nextInt(array.length);
         return array[rnd];
